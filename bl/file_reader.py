a1d94acc1488909d392e8efc21eabe4676995807
Zuk Levinson <zuklevinson@gmail.com>
2021-04-04 20:37:12 +0300

    feat(): playing with data

--- bl/file_reader.py
+++ bl/file_reader.py
@@ -1,16 +1,25 @@
 import os
-import pathlib
-import h5py
 import hdf5_getters
+import matplotlib.pyplot as plt
 
-
-
+dictionary = {}
 
 for path, subdirs, files in os.walk("MillionSongSubset"):
     for name in files:
         current_file_path = os.path.join(path, name)
 
         h5 = hdf5_getters.open_h5_file_read(current_file_path)
-        duration = hdf5_getters.get_duration(h5)
-        print(duration)
+
+        artist = hdf5_getters.get_artist_name(h5).decode('UTF-8')
+
+        if artist in dictionary:
+            dictionary[artist] += 1
+        else:
+            dictionary[artist] = 1
+
         h5.close()
+
+plt.bar(range(len(dictionary)), list(map(lambda x: r"{}".format(x), list(dictionary.values()))), align='center')
+plt.xticks(range(len(dictionary)))
+
+plt.show()
